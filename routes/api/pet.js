1b2148952af28bbaa6bdcfa9c801f24967cfcc25
Evan Arenas <Evan_Arenas@insideranken.org>
2021-09-28 16:24:02 -0500

    Updated pet-adoption-v2
    - Connected to mongoDb
    - Updated pet.js routes

--- routes/api/pet.js
+++ routes/api/pet.js
@@ -2,6 +2,8 @@ const debug = require('debug')('app:routes:api:pet');
 const debugError = require('debug')('app:error');
 const express = require('express');
 const { nanoid } = require('nanoid');
+const dbModule = require('../../database');
+const { newId } = require('../../database');
 
 const petsArray = [
   { _id: '1', name: 'Fido', createdDate: new Date() },
@@ -13,11 +15,23 @@ const petsArray = [
 const router = express.Router();
 
 // define routes
-router.get('/list', (req, res, next) => {
-  res.json(petsArray);
+router.get('/list', async (req, res, next) => {
+  try {
+    const pets = await dbModule.findAllPets();
+    res.json(pets);
+  } catch (err) {
+    next(err);
+  }
 });
-router.get('/:petId', (req, res, next) => {
-  const petId = req.params.petId;
+router.get('/:petId', async (req, res, next) => {
+  try {
+    const petId = newId(req.params.petId);
+    const pet = await dbModule.findPetById(petId);
+    if (!pet) res.status(404).json({ error: `PetId ${petId} not found` });
+    else res.status(200).json(pet);
+  } catch (err) {
+    next(err);
+  }
 
   // array lookup
   // const pet = petsArray[petId];
@@ -34,38 +48,38 @@ router.get('/:petId', (req, res, next) => {
   // res.json(pet);
 
   // using find
-  const pet = petsArray.find((x) => x._id == petId);
-  if (!pet) {
-    res.status(404).json({ error: 'Pet not found.' });
-  } else {
-    res.json(pet);
-  }
+  // const pet = petsArray.find((x) => x._id == petId);
+  // if (!pet) {
+  //   res.status(404).json({ error: 'Pet not found.' });
+  // } else {
+  //   res.json(pet);
+  // }
 });
-router.put('/new', (req, res, next) => {
-  const petId = nanoid();
-  const { species, name, gender } = req.body;
-  const age = parseInt(req.body.age);
+router.put('/new', async (req, res, next) => {
+  try {
+    const pet = {
+      _id: newId(),
+      species: req.body.species,
+      name: req.body.name,
+      age: parseInt(req.body.age),
+      gender: req.body.gender,
+      createdDate: new Date(),
+    };
 
-  const pet = {
-    _id: petId,
-    species, // species: species,
-    name,
-    age,
-    gender,
-    createdDate: new Date(),
-  };
-
-  if (!species) {
-    res.status(400).json({ error: 'Species required.' });
-  } else if (!name) {
-    res.status(400).json({ error: 'Name required.' });
-  } else if (!age) {
-    res.status(400).json({ error: 'Age required.' });
-  } else if (!gender) {
-    res.status(400).json({ error: 'Gender required.' });
-  } else {
-    petsArray.push(pet);
-    res.json(pet);
+    if (!pet.species) {
+      res.status(400).json({ error: 'Species required.' });
+    } else if (!pet.name) {
+      res.status(400).json({ error: 'Name required.' });
+    } else if (!pet.age) {
+      res.status(400).json({ error: 'Age required.' });
+    } else if (!pet.gender) {
+      res.status(400).json({ error: 'Gender required.' });
+    } else {
+      await dbModule.insertOnePet(pet);
+      res.json({ message: 'Pet inserted.' });
+    }
+  } catch (err) {
+    next(err);
   }
 });
 router.put('/:petId', (req, res, next) => {
